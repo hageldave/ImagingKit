61d3a873211426ebe28135aa2aa51ddd60456edb
David <david@RECHENKNECHTLNX>
2016-01-29 21:26:22 +0100

    adds Blend class for image and pixel blending

--- ImagingKit_Filter/src/main/java/hageldave/imagingkit/filter/Crop.java
+++ ImagingKit_Filter/src/main/java/hageldave/imagingkit/filter/Crop.java
@@ -6,11 +6,11 @@ import hageldave.imagingkit.core.Img;
 
 public class Crop {
 
-	public Img crop(Img img, int x, int y, int width, int height){
+	public static Img crop(Img img, int x, int y, int width, int height){
 		return img.copyArea(x, y, width, height, null, 0, 0);
 	}
 	
-	public BufferedImage crop(BufferedImage bufferedimg, int x, int y, int width, int height){
+	public static BufferedImage crop(BufferedImage bufferedimg, int x, int y, int width, int height){
 		Img img = null;
 		try {
 			img = Img.createRemoteImg(bufferedimg);
