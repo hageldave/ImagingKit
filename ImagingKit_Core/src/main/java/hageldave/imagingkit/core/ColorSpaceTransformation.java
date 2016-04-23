cea947e0d7deb16921bda80998c75c47ef9d88ae
unknown <david@JULESPC>
2016-04-23 21:02:23 +0200

    rewriting RGB_2_LAB

--- ImagingKit_Core/src/main/java/hageldave/imagingkit/core/ColorSpaceTransformation.java
+++ ImagingKit_Core/src/main/java/hageldave/imagingkit/core/ColorSpaceTransformation.java
@@ -2,39 +2,24 @@ package hageldave.imagingkit.core;
 
 import java.util.function.Consumer;
 
+
 public enum ColorSpaceTransformation {
 	/** TODO: javadocs for each */
-	RGB_2_XYZ(px->
+	RGB_2_LAB(px->
 	{
-		px.setARGB(px.a(), 
-				//             R      G     B
-				px.getGrey(   49,    31,   20),  // X
-				px.getGrey(17697, 81240, 1063),  // Y
-				px.getGrey(    0,     1,   99)); // Z
-	}),
-	XYZ_2_LAB(px->
-	{
-		float temp = labFunc(px.g()/255.0f);
-		px.setARGB(px.a(), 
-				(int) (255*temp),                               // L
-				(int) (127*(labFunc(px.r()/255.0f)-temp)+128),  // a
-				(int) (127*(temp-labFunc(px.b()/255.0f))+128)); // b
-	}),
-	LAB_2_XYZ(px->
-	{
-		float temp = px.r()/255.0f; // =L/255
-		px.setValue(Pixel.argb_bounded(px.a(), 
-				(int) (255*labFuncInv(temp + (px.g()-127)/128.0f)),   // X
-				(int) (255*labFuncInv(temp)),                         // Y
-				(int) (255*labFuncInv(temp - (px.b()-127)/128.0f)))); // Z
-	}),
-	XYZ_2_RGB(px->
-	{
-		px.setValue(Pixel.argb_bounded(px.a(),
-				//             X       Y        Z
-				px.getGrey(23646,  -8966,   -4680),   // R 
-				px.getGrey(-5151,  14264,     887),   // G
-				px.getGrey(   52,   -144,   10092))); // B
+		float x,y,z; x=y=z=0;
+		{// first convert to CIEXYZ (assuming sRGB color space with D65 white)
+			float r = px.r_normalized(); float g = px.g_normalized(); float b = px.b_normalized();
+			x = r*0.4124564f + g*0.3575761f + b*0.1804375f;
+			y = r*0.2126729f + g*0.7151522f + b*0.0721750f;
+			z = r*0.0193339f + g*0.1191920f + b*0.9503041f;
+		}
+		float L,a,b; L=a=b=0;
+		{// now convert to Lab
+			float temp = LAB.func(y/LAB.Yn);
+			L = 116*temp-16;
+			a = 500*(LAB.func(x/LAB.Xn)-temp);
+		}
 	}),
 	RGB_2_HSV(px->
 	{
@@ -57,7 +42,8 @@ public enum ColorSpaceTransformation {
 			px.setARGB(px.a(),(int)r,(int)g,(int)b);
 		}
 	}),
-	HSV_2_RGB(px->{
+	HSV_2_RGB(px->
+	{
 		float h = px.r_normalized()*359;
 		float s = px.g_normalized();
 		float v = px.b_normalized();
@@ -85,19 +71,21 @@ public enum ColorSpaceTransformation {
 	}
 	
 	
-	//// STATIC ////
-	private static final float lab6_29 = 6.0f/29.0f;
-	private static final float lab6_29_3 = lab6_29*lab6_29*lab6_29;
-	private static final float lab1_3_29_6_2 = (1.0f/3.0f) * (29.0f/6.0f) * (29.0f/6.0f);
-	
-	private static float labFunc(float q){
-//		return q > lab6_29_3 ? (float)Math.cbrt(q):lab1_3_29_6_2*q + (4.0f/29.0f);
-		return (float) Math.cbrt(q);
+	////STATIC ////
+	private static final class LAB {
+		static final float Xn = 95.047f;
+		static final float Yn = 100f;
+		static final float Zn = 108.883f;
+		static final float lab6_29 = 6.0f/29.0f;
+		static final float lab6_29_3 = lab6_29*lab6_29*lab6_29;
+		static final float lab1_3_29_6_2 = (1.0f/3.0f) * (29.0f/6.0f) * (29.0f/6.0f);
+
+		static float func(float q){
+			return q > lab6_29_3 ? (float)Math.cbrt(q):lab1_3_29_6_2*q + (4.0f/29.0f);
+		}
+
+		static float funcInv(float q){
+			return q > lab6_29 ? q*q*q : 3*lab6_29*lab6_29*(q-(4.0f/29.0f));
+		}
 	}
-	
-	private static float labFuncInv(float q){
-//		return q > lab6_29 ? q*q*q : 3*lab6_29*lab6_29*(q-(4.0f/29.0f));
-		return q*q*q;
-	}
-	
 }
