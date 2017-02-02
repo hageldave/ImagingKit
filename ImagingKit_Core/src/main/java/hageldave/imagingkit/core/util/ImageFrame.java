5878a7a630d069d8f13a0ffaa9eb1c1bc99c8484
David <david@RECHENKNECHTLNX>
2017-02-02 19:30:14 +0100

    minor refactorings in ImageFrame; also some javadocs

--- ImagingKit_Core/src/main/java/hageldave/imagingkit/core/util/ImageFrame.java
+++ ImagingKit_Core/src/main/java/hageldave/imagingkit/core/util/ImageFrame.java
@@ -10,37 +10,94 @@ import hageldave.imagingkit.core.Img;
 
 import static hageldave.imagingkit.core.TheImageObserver.*;
 
+/**
+ * {@link JFrame} for displaying an image utilizing an {@link ImagePanel}.
+ * This class is merely meant for debugging purposes when you want
+ * to quickly view an image without the need to export it as an
+ * image file.
+ * <p>
+ * Use the static method {@link #display(Image)} or {@link #display(Img)}
+ * to quickly obtain an ImageFrame that displays the specified image.
+ * 
+ * @author hageldave
+ * @since 1.4
+ */
 @SuppressWarnings("serial")
 public class ImageFrame extends JFrame {
 	
-	ImagePanel panel;
+	/** the {@link ImagePanel} used by this ImageFrame 
+	 * @since 1.4 */
+	protected ImagePanel panel;
 	
+	/**
+	 * Constructs a new ImagePanel.<br>
+	 * The Frame is initialized via the argument less JFrame constructor
+	 * {@link JFrame#JFrame()}.<br>
+	 * An {@link ImagePanel} is added to the frame's content pane.
+	 * The ImagePanel can be accessed using {@link #getPanel()}.
+	 * <p>
+	 * For a quick setup of this frame the {@link #useDefaultSettings()}
+	 * method can be used to make this frame ready to go.
+	 * 
+	 * @since 1.4
+	 */
 	public ImageFrame() {
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		super();
 		this.panel = new ImagePanel();
 		this.getContentPane().add(this.panel);
-		this.setMinimumSize(new Dimension(300,300));
-		this.pack();
 	}
 	
-	public void setImg(final Image img){
+	/**
+	 * Sets default close operation to {@link JFrame#EXIT_ON_CLOSE}, <br>
+	 * sets the minimum size to 300x300 <br>
+	 * and calls {@link #pack()}.
+	 * @return this for chaining.
+	 * 
+	 * @since 1.4
+	 */
+	public ImageFrame useDefaultSettings(){
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setMinimumSize(new Dimension(300,300));
+		this.pack();
+		return this;
+	}
+	
+	/**
+	 * Sets the image to be displayed by this frame.
+	 * This will update the panel (calls repaint()).
+	 * @param img to be displayed
+	 * 
+	 * @see #setImg(Img)
+	 * @since 1.4
+	 */
+	public void setImage(final Image img){
 		panel.setImage(img);
 		this.setTitle(img.getWidth(OBS_WIDTHHEIGHT) + " x " + img.getHeight(OBS_WIDTHHEIGHT) + " Pixels");
 	}
 	
+	/**
+	 * Sets the image to be displayed by this frame.
+	 * This will update the panel (calls repaint()).
+	 * @param img to be displayed
+	 * 
+	 * @see #setImage(Image)
+	 * @since 1.4
+	 */
 	public void setImg(final Img img){
-		this.setImg(img.getRemoteBufferedImage());
+		this.setImage(img.getRemoteBufferedImage());
 	}
 	
+
 	public static ImageFrame display(final Image img){
-		ImageFrame frame = new ImageFrame();
+		ImageFrame frame = new ImageFrame().useDefaultSettings();
 		SwingUtilities.invokeLater( () -> {
-			frame.setImg(img);
+			frame.setImage(img);
 			frame.setVisible(true);
 		});
 		return frame;
 	}
 	
+
 	public static ImageFrame display(final Img img){
 		return display(img.getRemoteBufferedImage());
 	}
