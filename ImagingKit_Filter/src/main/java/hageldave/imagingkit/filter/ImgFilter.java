a7e3d5c1a13b6d094e87026c59b576402dc49dfb
hageldave <haegele.david@gmail.com>
2017-03-06 01:03:22 +0100

    continued imgfilter test; added javadocs for ImgFilter

--- ImagingKit_Filter/src/main/java/hageldave/imagingkit/filter/ImgFilter.java
+++ ImagingKit_Filter/src/main/java/hageldave/imagingkit/filter/ImgFilter.java
@@ -4,19 +4,94 @@ import java.util.Objects;
 
 import hageldave.imagingkit.core.Img;
 
+/**
+ * The base interface for image filters.
+ * It provides the <tt>applyTo</tt> methods where 
+ * {@link #applyTo(Img, boolean, int, int, int, int)} has to be implemented,
+ * while the others have default implementations.<br>
+ * It also provides a default implementation of the chaining method
+ * {@link #followedBy(ImgFilter)}.
+ * 
+ * @author hageldave
+ */
 public interface ImgFilter {
 	
+	/**
+	 * Applies this filter to the whole specified {@link Img}.
+	 * The filter will prefer executing a sequential implementation (not parallel).
+	 * 
+	 * @param img the filter will be applied to
+	 */
 	public default void applyTo(Img img)
 		{applyTo(img, false);}
 
+	/**
+	 * Applies this filter to the whole specified {@link Img}.
+	 * When parallelPreferred is true, a multithreaded (parallel) 
+	 * implementation will be executed if possible.<br>
+	 * Filters implementing this interface do not guarantee to provide a
+	 * parallel implementation.
+	 * 
+	 * @param img the filter will be applied to
+	 * @param parallelPreferred if true, the filter will be executed in parallel 
+	 * if possible
+	 * 
+	 * @see #applyTo(Img, boolean, int, int, int, int)
+	 * @see #applyTo(Img, int, int, int, int)
+	 * @see #applyTo(Img)
+	 */
 	public default void applyTo(Img img, boolean parallelPreferred)
 		{applyTo(img, parallelPreferred, 0, 0, img.getWidth(), img.getHeight());}
 	
+	/**
+	 * Applies this filter to the specified {@link Img} within the specified
+	 * area (x, y, width, height). The filter will prefer executing a sequential 
+	 * implementation (not parallel).
+	 * 
+	 * @param img the filter will be applied to
+	 * @param x starting x coordinate of the area the filter will be applied to
+	 * @param y starting y coordinate of the area the filter will be applied to
+	 * @param width of the area the filter will be applied to
+	 * @param height of the area the filter will be applied to
+	 * 
+	 * @see #applyTo(Img, boolean, int, int, int, int)
+	 * @see #applyTo(Img, boolean)
+	 * @see #applyTo(Img)
+	 */
 	public default void applyTo(Img img, int x, int y, int width, int height)
 		{applyTo(img, false, 0, 0, img.getWidth(), img.getHeight());}
 	
+	/**
+	 * Applies this filter to the specified {@link Img} within the specified
+	 * area (x, y, width, height). When parallelPreferred is true, a 
+	 * multithreaded (parallel) implementation will be executed if possible.<br>
+	 * Filters implementing this interface do not guarantee to provide a
+	 * parallel implementation.
+	 * 
+	 * @param img the filter will be applied to
+	 * @param parallelPreferred if true, the filter will be executed in parallel 
+	 * if possible
+	 * @param x starting x coordinate of the area the filter will be applied to
+	 * @param y starting y coordinate of the area the filter will be applied to
+	 * @param width of the area the filter will be applied to
+	 * @param height of the area the filter will be applied to
+	 * 
+	 * @see #applyTo(Img, int, int, int, int)
+	 * @see #applyTo(Img, boolean)
+	 * @see #applyTo(Img)
+	 */
+	/* >>>> TO BE IMPLEMENTED <<<< */
 	public void applyTo(Img img, boolean parallelPreferred, int x, int y, int width, int height);
 	
+	/**
+	 * Returns a combined ImageFilter that will apply the specified filter 
+	 * right after this one.
+	 * 
+	 * @param nextFilter to be applied after this filter
+	 * @return an ImageFilter that will execute this filter and the specified 
+	 * one subsequently
+	 * @throws NullPointerException if nextFilter is null.
+	 */
 	public default ImgFilter followedBy(ImgFilter nextFilter){
 		Objects.requireNonNull(nextFilter);
 		return new ImgFilter() {
--- ImagingKit_Filter/src/test/java/hageldave/imagingkit/filter/ImgFilterTest.java
+++ ImagingKit_Filter/src/test/java/hageldave/imagingkit/filter/ImgFilterTest.java
@@ -82,13 +82,35 @@ public class ImgFilterTest {
 	
 	@Test
 	public void testMixedFollowedBy(){
+		
+		Img[] theCopyUsed = {null,null};
+		
 		NeighborhoodFilter shiftRight = NeighborhoodFilter.fromConsumer((px,copy)->{
 			px.setValue(copy.getValue(px.getX()-2, px.getY(), Img.boundary_mode_repeat_image));
+			if(px.getIndex() == 0)
+				theCopyUsed[0] = copy;
 		});
 		NeighborhoodFilter shiftLeft = NeighborhoodFilter.fromConsumer((px,copy)->{
 			px.setValue(copy.getValue(px.getX()+2, px.getY(), Img.boundary_mode_repeat_image));
+			if(px.getIndex() == 0)
+				theCopyUsed[1] = copy;
 		});
 		PerPixelFilter rotateChannels = PerPixelFilter.fromPixelConsumer(px->px.setRGB_preserveAlpha(px.g(),px.b(),px.r()));
+		
+		Img img = new Img(10,10);
+		img.setValue(4, 4, 0x000001);
+		shiftRight.applyTo(img);
+		assertEquals(0, img.getValue(4, 4));
+		assertEquals(0x000001, img.getValue(6, 4));
+		
+		NeighborhoodFilter combined = shiftRight.followedBy(rotateChannels).followedBy(shiftLeft);
+		combined.applyTo(img);
+		
+		assertEquals(0, img.getValue(4, 4));
+		assertEquals(0, img.getValue(8, 4));
+		assertEquals(0x000100, img.getValue(6, 4));
+		assertEquals(theCopyUsed[0], theCopyUsed[1]);
+		
 	}
 	
 }
