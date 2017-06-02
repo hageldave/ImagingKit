5ebc16bf28cd2d82c348133053f73b98f4ab2a2b
haegeldd <haegele.david@gmail.com>
2017-06-02 12:03:03 +0200

    imageframe setimage will no longer set the title. this way the title can remain even when changing the image

--- ImagingKit_Core/src/main/java/hageldave/imagingkit/core/util/ImageFrame.java
+++ ImagingKit_Core/src/main/java/hageldave/imagingkit/core/util/ImageFrame.java
@@ -1,22 +1,22 @@
 /*
  * Copyright 2017 David Haegele
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to 
- * deal in the Software without restriction, including without limitation the 
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
- * sell copies of the Software, and to permit persons to whom the Software is 
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in 
+ *
+ * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
 
@@ -30,8 +30,6 @@ import javax.swing.SwingUtilities;
 
 import hageldave.imagingkit.core.Img;
 
-import static hageldave.imagingkit.core.TheImageObserver.*;
-
 /**
  * {@link JFrame} for displaying an image utilizing an {@link ImagePanel}.
  * This class is merely meant for debugging purposes when you want
@@ -42,17 +40,17 @@ import static hageldave.imagingkit.core.TheImageObserver.*;
  * to quickly obtain an ImageFrame that displays the specified image.
  * <p>
  * Use {@link #getPanel()} to access the frame's ImagePanel.
- * 
+ *
  * @author hageldave
  * @since 1.4
  */
 @SuppressWarnings("serial")
 public class ImageFrame extends JFrame {
-	
-	/** the {@link ImagePanel} used by this ImageFrame 
+
+	/** the {@link ImagePanel} used by this ImageFrame
 	 * @since 1.4 */
 	protected ImagePanel panel;
-	
+
 	/**
 	 * Constructs a new ImagePanel.<br>
 	 * The Frame is initialized via the argument less JFrame constructor
@@ -62,7 +60,7 @@ public class ImageFrame extends JFrame {
 	 * <p>
 	 * For a quick setup of this frame the {@link #useDefaultSettings()}
 	 * method can be used to make this frame ready to go.
-	 * 
+	 *
 	 * @since 1.4
 	 */
 	public ImageFrame() {
@@ -70,13 +68,13 @@ public class ImageFrame extends JFrame {
 		this.panel = new ImagePanel();
 		this.getContentPane().add(this.panel);
 	}
-	
+
 	/**
 	 * Sets default close operation to {@link JFrame#EXIT_ON_CLOSE}, <br>
 	 * sets the minimum size to 300x300 <br>
 	 * and calls {@link #pack()}.
 	 * @return this for chaining.
-	 * 
+	 *
 	 * @since 1.4
 	 */
 	public ImageFrame useDefaultSettings(){
@@ -85,32 +83,32 @@ public class ImageFrame extends JFrame {
 		this.pack();
 		return this;
 	}
-	
+
 	/**
 	 * Sets the image to be displayed by this frame.
 	 * This will update the panel (calls repaint()).
 	 * @param img to be displayed
-	 * 
+	 *
 	 * @see #setImg(Img)
 	 * @since 1.4
 	 */
-	public void setImage(final Image img){
+	public ImageFrame setImage(final Image img){
 		panel.setImage(img);
-		this.setTitle(img.getWidth(OBS_WIDTHHEIGHT) + " x " + img.getHeight(OBS_WIDTHHEIGHT) + " Pixels");
+		return this;
 	}
-	
+
 	/**
 	 * Sets the image to be displayed by this frame.
 	 * This will update the panel (calls repaint()).
 	 * @param img to be displayed
-	 * 
+	 *
 	 * @see #setImage(Image)
 	 * @since 1.4
 	 */
-	public void setImg(final Img img){
-		this.setImage(img.getRemoteBufferedImage());
+	public ImageFrame setImg(final Img img){
+		return this.setImage(img.getRemoteBufferedImage());
 	}
-	
+
 	/**
 	 * Displays the specified image in a new ImageFrame instance.
 	 * <p>
@@ -118,7 +116,7 @@ public class ImageFrame extends JFrame {
 	 * call to <tt>setVisible(true)</tt> on the AWT event dispatching thread.
 	 * @param img to be displayed
 	 * @return the frame that displays the image.
-	 * 
+	 *
 	 * @see #display(Img)
 	 * @since 1.4
 	 */
@@ -130,7 +128,7 @@ public class ImageFrame extends JFrame {
 		});
 		return frame;
 	}
-	
+
 	/**
 	 * Displays the specified image in a new ImageFrame instance.
 	 * <p>
@@ -138,14 +136,14 @@ public class ImageFrame extends JFrame {
 	 * call to <tt>setVisible(true)</tt> on the AWT event dispatching thread.
 	 * @param img to be displayed
 	 * @return the frame that displays the image.
-	 * 
+	 *
 	 * @see #display(Image)
 	 * @since 1.4
 	 */
 	public static ImageFrame display(final Img img){
 		return display(img.getRemoteBufferedImage());
 	}
-	
+
 	/**
 	 * Returns the {@link ImagePanel} of this frame.
 	 * @return the image panel
