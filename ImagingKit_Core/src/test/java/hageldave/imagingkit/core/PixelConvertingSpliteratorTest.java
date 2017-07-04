ded6b53e01538f9535d77c4b6e25d4e41ac0e177
David <david@RECHENKNECHTLNX>
2017-07-04 02:08:16 +0200

    tests for img.foreach(converter.. and img.stream(converter...

--- ImagingKit_Core/src/test/java/hageldave/imagingkit/core/PixelConvertingSpliteratorTest.java
+++ ImagingKit_Core/src/test/java/hageldave/imagingkit/core/PixelConvertingSpliteratorTest.java
@@ -1,7 +1,6 @@
 package hageldave.imagingkit.core;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.util.LinkedList;
 import java.util.Spliterator;
@@ -45,6 +44,7 @@ public class PixelConvertingSpliteratorTest {
 				iter.forEachRemaining(rotateChannels);
 			}
 			for(int i = 0; i < img.numValues(); i++){
+				// g channel will contain special value
 				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.b(img.getData()[i]));
 				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 1+(i%0xfe), Pixel.g(img.getData()[i]));
 				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.r(img.getData()[i]));
@@ -57,14 +57,76 @@ public class PixelConvertingSpliteratorTest {
 			StreamSupport.stream(split, true).forEach(rotateChannels);
 
 			for(int i = 0; i < img.numValues(); i++){
+				// now r channel will contain special value
 				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.b(img.getData()[i]));
 				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 1+(i%0xfe), Pixel.r(img.getData()[i]));
 				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.g(img.getData()[i]));
 			}
 		}
 
+		
+		PixelConverter<double[]> converter = PixelConverter.fromFunctions(
+				()->new double[3], 
+				(px,a)->{a[0]=px.r_normalized();a[1]=px.g_normalized();a[2]=px.b_normalized();}, 
+				(a,px)->{px.setRGB_fromNormalized_preserveAlpha(a[0], a[1], a[2]);});
+		
+		
 		{
-			PixelConverter<double[]> converter = PixelConverter.fromFunctions(()->new double[3], null, null);
+			img.forEach(converter, false, rotateChannels);
+			for(int i = 0; i < img.numValues(); i++){
+				// b now
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.r(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 1+(i%0xfe), Pixel.b(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.g(img.getData()[i]));
+			}
+			img.forEach(converter, true, rotateChannels);
+			for(int i = 0; i < img.numValues(); i++){
+				// g again
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.r(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 1+(i%0xfe), Pixel.g(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.b(img.getData()[i]));
+			}
+			img.forEach(converter, false, 0, 0, img.getWidth(), 1, rotateChannels);
+			for(int i = img.getWidth(); i < img.numValues(); i++){
+				// g for all except first row
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.r(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 1+(i%0xfe), Pixel.g(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.b(img.getData()[i]));
+			}
+			for(int i = 0; i < img.getWidth(); i++){
+				// r for first row
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.g(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 1+(i%0xfe), Pixel.r(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.b(img.getData()[i]));
+			}
+			img.forEach(converter, true, 0, 0, img.getWidth(), 1, rotateChannels);
+			for(int i = img.getWidth(); i < img.numValues(); i++){
+				// g for all except first row
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.r(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 1+(i%0xfe), Pixel.g(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.b(img.getData()[i]));
+			}
+			for(int i = 0; i < img.getWidth(); i++){
+				// b for first row
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.g(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 1+(i%0xfe), Pixel.b(img.getData()[i]));
+				assertEquals("i="+i+" "+Integer.toHexString(img.getData()[i]), 0, Pixel.r(img.getData()[i]));
+			}
+		}
+		
+		{
+			double sumb1 = img.stream().mapToDouble(px->px.b_normalized()).sum();
+			double sumb2 = img.stream(converter, false).mapToDouble(a->a[2]).sum();
+			assertNotEquals(0, sumb1, 0);
+			assertEquals(sumb1, sumb2, 0);
+			double sumg1 = img.stream().mapToDouble(px->px.g_normalized()).sum();
+			double sumg2 = img.stream(converter, true).mapToDouble(a->a[1]).sum();
+			assertNotEquals(0, sumg1, 0);
+			assertEquals(sumg1, sumg2, 0);
+			double sumw1 = img.stream(0, 0, img.getWidth(), 1).mapToDouble(px->px.r_normalized()+px.g_normalized()+px.b_normalized()).sum();
+			double sumw2 = img.stream(converter, false, 0, 0, img.getWidth(), 1).mapToDouble(a->a[0]+a[1]+a[2]).sum();
+			assertNotEquals(0, sumw1, 0);
+			assertEquals(sumw1, sumw2, 0);
 		}
 
 	}
